7a011b0883b1ad96798aa4a88958f97c63da1f5d
xycu <smonindemid@gmail.com>
2026-08-01 20:14:31 +0500

    feat: Move the main page to the app

--- src/App.vue
+++ src/App.vue
@@ -35,6 +35,87 @@
                 </div>
             </div>
         </div>
+
+        <section class="about">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 offset-lg-3">
+                        <div class="title">About Us</div>
+                        <img class="beanslogo" src="@/assets/logo/Beans_logo_dark.svg" alt="Beans logo">
+                        <div class="about__text">
+                            Extremity sweetness difficult behaviour he of. On disposal of as landlord horrible.
+                            Afraid at highly months do things on at. Situation recommend objection do intention
+                            so questions. As greatly removed calling pleased improve an. Last ask him cold feel
+                            met spot shy want. Children me laughing we prospect answered followed. At it went
+                            is song that held help face.<br><br>
+
+                            Now residence dashwoods she excellent you. Shade being under his bed her, Much
+                            read on as draw. Blessing for ignorant exercise any yourself unpacked. Pleasant
+                            horrible but confined day end marriage. Eagerness furniture set preserved far
+                            recommend. Did even but nor are most gave hope. Secure active living depend son
+                            repair day ladies now.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="best">
+            <div class="container">
+                <div class="title">Our best</div>
+                <div class="row">
+                    <div class="col-lg-10 offset-lg-1">
+                        <div class="best__wrapper">
+                            <div class="best__item">
+                                <img src="@/assets/img/coffee-1.jpg" alt="coffee">
+                                <div class="best__item-title">
+                                    Solimo Coffee Beans 2kg
+                                </div>
+                                <div class="best__item-price">10.73$</div>
+                            </div>
+                            <div class="best__item">
+                                <img src="@/assets/img/coffee-2.jpg" alt="coffee">
+                                <div class="best__item-title">
+                                    Presto Coffee Beans 1kg
+                                </div>
+                                <div class="best__item-price">15.99$</div>
+                            </div>
+                            <div class="best__item">
+                                <img src="@/assets/img/coffee-3.jpg" alt="coffee">
+                                <div class="best__item-title">
+                                    AROMISTICO Coffee 1kg
+                                </div>
+                                <div class="best__item-price">6.99$</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 offset-lg-3">
+                        <ul class="footer d-flex flex-wrap">
+                            <li class=" footer__item">
+                                <a href="./index.html">
+                                    <img src="@/assets/logo/Logo_black.svg" alt="logo">
+                                </a>
+                            </li>
+                            <li class="footer__item">
+                                <a href="./coffeepage.html">Our coffee</a>
+                            </li>
+                            <li class="footer__item">
+                                <a href="./goodspage.html">For your pleasure</a>
+                            </li>
+                            <li class="footer__item">
+                                <a href="./contacts.html">Contact us</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <img class="beanslogo" src="@/assets/logo/Beans_logo_dark.svg" alt="Beans logo">
+            </div>
+        </footer>
   </main>
 </template>
 
