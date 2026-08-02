e9ace560ab6da52443797a93b68c2fd0fc9db512
xycu <smonindemid@gmail.com>
2026-08-02 14:30:14 +0500

    feat: add a product card component

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="best__wrapper">
+    <div class="best__item">
+      <img src="@/assets/img/coffee-1.jpg" alt="coffee" />
+      <div class="best__item-title">Solimo Coffee Beans 2kg</div>
+      <div class="best__item-price">10.73$</div>
+    </div>
+    <div class="best__item">
+      <img src="@/assets/img/coffee-2.jpg" alt="coffee" />
+      <div class="best__item-title">Presto Coffee Beans 1kg</div>
+      <div class="best__item-price">15.99$</div>
+    </div>
+    <div class="best__item">
+      <img src="@/assets/img/coffee-3.jpg" alt="coffee" />
+      <div class="best__item-title">AROMISTICO Coffee 1kg</div>
+      <div class="best__item-price">6.99$</div>
+    </div>
+  </div>
+</template>
--- src/views/HeroView.vue
+++ src/views/HeroView.vue
@@ -47,29 +47,7 @@
                 <div class="title">Our best</div>
                 <div class="row">
                     <div class="col-lg-10 offset-lg-1">
-                        <div class="best__wrapper">
-                            <div class="best__item">
-                                <img src="@/assets/img/coffee-1.jpg" alt="coffee">
-                                <div class="best__item-title">
-                                    Solimo Coffee Beans 2kg
-                                </div>
-                                <div class="best__item-price">10.73$</div>
-                            </div>
-                            <div class="best__item">
-                                <img src="@/assets/img/coffee-2.jpg" alt="coffee">
-                                <div class="best__item-title">
-                                    Presto Coffee Beans 1kg
-                                </div>
-                                <div class="best__item-price">15.99$</div>
-                            </div>
-                            <div class="best__item">
-                                <img src="@/assets/img/coffee-3.jpg" alt="coffee">
-                                <div class="best__item-title">
-                                    AROMISTICO Coffee 1kg
-                                </div>
-                                <div class="best__item-price">6.99$</div>
-                            </div>
-                        </div>
+                        <CardComponent />
                     </div>
                 </div>
             </div>
@@ -80,9 +58,10 @@
 
 <script>
 import NavBarComponent from '../components/NavBarComponent.vue';
+import CardComponent from '../components/CardComponent.vue';
 
 export default {
-    components: {NavBarComponent}
+    components: {NavBarComponent, CardComponent}
 }
 
 </script>
