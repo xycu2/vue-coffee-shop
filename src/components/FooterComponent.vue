00f033a905f92370a8f1fed91bef2c50b045494a
xycu <smonindemid@gmail.com>
2026-08-04 12:26:26 +0500

    feat: Add data to the footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -5,16 +5,18 @@
         <div class="col-lg-6 offset-lg-3">
           <ul class="footer d-flex flex-wrap">
             <li class="footer__item">
-              <router-link to="/"><img src="@/assets/logo/Logo_black.svg" alt="logo" /></router-link>
+              <router-link :to="links[0].link">
+                <img :src="require(`@/assets/logo/${links[0].icon}`)" :alt="links[0].icon" />
+              </router-link>
             </li>
             <li class="footer__item">
-              <router-link to="our-coffee">Our coffee</router-link>
+              <router-link :to="links[1].link">{{ links[1].text }}</router-link>
             </li>
             <li class="footer__item">
-              <router-link to="for-pleasure">For your pleasure</router-link>
+              <router-link :to="links[2].link">{{ links[2].text }}</router-link>
             </li>
             <li class="footer__item">
-              <router-link to="contact-us">Contact us</router-link>
+              <router-link :to="links[3].link">{{ links[3].text }}</router-link>
             </li>
           </ul>
         </div>
@@ -27,3 +29,35 @@
     </div>
   </footer>
 </template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      links: [
+        {
+          id: 0,
+          link: '/',
+          icon: 'Logo_black.svg'
+        },
+        {
+          id: 1,
+          text: 'Our coffee',
+          link: '/our-coffee',
+        },
+        {
+          id: 2,
+          text: 'For your pleasure',
+          link: 'for-pleasure',
+        },
+        {
+          id: 3,
+          text: 'Contact us',
+          link: 'contact-us',
+        }
+      ]
+    }
+  }
+}
+</script>
