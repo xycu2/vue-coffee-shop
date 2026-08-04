9251355891951a79a9243b595dace200aad7ffa3
xycu <smonindemid@gmail.com>
2026-08-04 13:27:24 +0500

    feat: Add a date to the cards in the Our Coffee section.

--- src/views/OurCoffeeView.vue
+++ src/views/OurCoffeeView.vue
@@ -73,6 +73,48 @@ import NavBarComponent from '../components/NavBarComponent.vue';
 import ProductCard from '../components/ProductCard.vue';
 
 export default {
-    components: {NavBarComponent, ProductCard}
+    components: {NavBarComponent, ProductCard},
+    data() {
+        return {
+            cards: [
+                {
+                    id: 0,
+                    text: 'Solimo Coffee Beans 2kg',
+                    icon: 'coffee-1.jpg',
+                    price: '10.73$',
+                },
+                {
+                    id: 1,
+                    text: 'Presto Coffee Beans 1kg',
+                    icon: 'coffee-2.jpg',
+                    price: '15.99$',
+                },
+                {
+                    id: 2,
+                    text: 'AROMISTICO Coffee 1kg',
+                    icon: 'coffee-3.jpg',
+                    price: '6.99$',
+                },
+                {
+                    id: 3,
+                    text: 'Solimo Coffee Beans 2kg',
+                    icon: 'coffee-1.jpg',
+                    price: '10.73$',
+                },
+                {
+                    id: 4,
+                    text: 'Presto Coffee Beans 1kg',
+                    icon: 'coffee-2.jpg',
+                    price: '15.99$',
+                },
+                {
+                    id: 5,
+                    text: 'AROMISTICO Coffee 1kg',
+                    icon: 'coffee-3.jpg',
+                    price: '6.99$',
+                }
+            ]
+        }
+    }
 }
 </script>
